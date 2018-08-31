4f297efc0c7c6e5230d560f99f6425fba3721c91
Jose Leon <leonj1@gmail.com>
2018-08-31 10:02:49 -0400

    Initial commit

---
+++ src/main/java/com/kandm/App.java
@@ -0,0 +1,11 @@
+package com.kandm;
+
+/**
+ * This service is meant to be the interface to Amazon S3
+ * This is to prevent every micro service in dapidi-land from having to have the same code
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
